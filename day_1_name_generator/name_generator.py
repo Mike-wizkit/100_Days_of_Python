dc07b65cb9a12070e46b4d7b0b7e3a076120d56d
Mike-Wizkt <wizkit.it@gmail.com>
2022-07-04 12:01:26 +0200

    first day 100 days of Python
    made a Gamers name generator
    with print and variables

---
+++ day_1_name_generator/name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to my Gamers name generator")
+city = input("What is the name of the city you grew up in? \n")
+pet = input("What is the name of your beloved pet? \n")
+print("your Gamers name could be " + city + " " + pet)
