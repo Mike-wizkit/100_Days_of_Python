e861f5d341cccf7bd50817da73dbcae067796120
Mike-Wizkt <wizkit.it@gmail.com>
2022-07-04 17:40:52 +0200

    tip calculator first printed the welcome message than asked for the total amount through a float input variable for the tip percentage and people ill used an integer input took the tip input and divided that times a hundred made a total tip var with the bill and tip input multiplied that divided over the amount of people printed the outcome

---
+++ day_2/tip_calculator.py
@@ -0,0 +1,11 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? € "))
+tip = int(input("How much tip would you like to give? 10%, 12%, or 15%? "))
+people = int(input("How many people to split the bill with? "))
+
+tip_in_percent = tip / 100
+total_tip = bill * tip_in_percent
+total_bill = bill + total_tip
+total_per_person = total_bill / people
+final_amount = "{:.2f}".format(total_per_person)
+print(f"Each person should pay: €{final_amount}")
