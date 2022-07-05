39929e80e5440a3c914c50acd6a0e7ee225a1f0d
Mike-Wizkt <wizkit.it@gmail.com>
2022-07-05 20:45:53 +0200

    spelling update

--- day_3/BMI_Calculator_2.0.py
+++ day_3/BMI_Calculator_2.0.py
@@ -1,5 +1,5 @@
 height = float(input("Please enter your height in m: "))
-weight = float(input("Please neter your weight in kg: "))
+weight = float(input("Please enter your weight in kg: "))
 bmi = round(weight / height ** 2)
 if bmi < 18.5:
   print(f"Your BMI is {bmi}, you are underweight.")
