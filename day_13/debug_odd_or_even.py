720c3965ef6e76d3d191662a26cf05a7c9633313
Mike-Wizkt <wizkit.it@gmail.com>
2022-07-15 19:11:12 +0200

    day 13 & 14 (100 days of python) day 13 was debugging and day 14 made a high and low game

---
+++ day_13/debug_odd_or_even.py
@@ -0,0 +1,6 @@
+number = int(input("Which number do you want to check?"))
+# original was if number % 2 = 0: and wrong indentations
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
